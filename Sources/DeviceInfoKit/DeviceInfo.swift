6f127bf1f7bf97e5116b2fcc768be53565fc9c09
Nick Kohrn <bnkohrn@icloud.com>
2023-05-17 10:06:21 -0400

    Add device locale

--- Sources/DeviceInfoKit/DeviceInfo.swift
+++ Sources/DeviceInfoKit/DeviceInfo.swift
@@ -4,14 +4,17 @@ public struct DeviceInfo: Equatable {
     public var osNameString: String
     public var osVersionString: String
     public var deviceModelString: String
+    public var locale: String?
 
     public init(
         osNameString: String = "",
         osVersionString: String = "",
-        deviceModelString: String = ""
+        deviceModelString: String = "",
+        locale: String? = nil
     ) {
         self.osNameString = osNameString
         self.osVersionString = osVersionString
         self.deviceModelString = deviceModelString
+        self.locale = locale
     }
 }
--- Sources/DeviceInfoKit/DeviceInfoClient.swift
+++ Sources/DeviceInfoKit/DeviceInfoClient.swift
@@ -2,5 +2,5 @@ import Foundation
 import UIKit
 
 public struct DeviceInfoClient {
-    public var info: (_ device: UIDevice) async -> DeviceInfo
+    public var info: (_ device: UIDevice, _ locale: Locale) async -> DeviceInfo
 }
